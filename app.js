32f999ff1587e30c8addafe92ece6f115213c544
zhangdi@ts-it.cn <zhangdi@ts-it.cn>
2022-06-23 17:28:09 +0800

    fix: 修改基础配置

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
-//使用express构建web服务器
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const session = require("express-session");
 const cors=require("cors");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-const home=require("./routes/home.js");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 /*引入路由模块*/
-var server = express();
-server.listen(8080);
-server.use(cors({
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.listen(8080);
+//从此所有响应，自动带Access-Control-Allow-Origin
+//万一客户端浏览器地址发生变化，只改这里origin一处即可！
+app.use(cors({
   origin:["http://127.0.0.1:3000","http://localhost:3000","http://localhost:8000",],
   credentials:true
 }));
-//从此所有响应，自动带Access-Control-Allow-Origin
-//万一客户端浏览器地址发生变化，只改这里origin一处即可！
+
 //配置session
-server.use(session({
+app.use(session({
   secret:"nodeServer",
   resave:true,
   saveUninitialized:true
 }));
-console.log('服务器启动完成,端口号8080');
 
-server.post('/',function(req,res){
+app.post('/',function(req,res){
 	res.send('成功')
 });
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 //使用body-parser中间件
-server.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public目录下
-server.use(express.static('public'));
+app.use(express.static('public'));
 /*使用路由器来管理路由*/
-server.use("/home",home);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
 
 
